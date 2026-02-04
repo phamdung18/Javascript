3da5e88ed5e4ca5ba1569953fdd82d9610b2d348
phamdung18 <vietdungpham182@gmail.com>
2026-02-04 13:44:15 +0700

    ktra 30p đầu giờ

--- ktra đầu giờ SS6/bai2.js
+++ ktra đầu giờ SS6/bai2.js
@@ -4,8 +4,8 @@ for (let index in money){
 }
 
 let sum = 0;
-for (let i = 0; i < money.length; i++){
-  if (i % 2 === 0) {
+for (let i=0;i<money.length;i++){
+  if (i %2===0) {
     sum += money[i];
   }
 }
